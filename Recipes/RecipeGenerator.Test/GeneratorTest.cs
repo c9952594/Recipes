2492f2a9abcddfcd9215bbc2e3893a610ad96d05
Craig Norton <c9952594@gmail.com>
2018-09-28 00:10:56 +0100

    Initial solution created

---
+++ Recipes/RecipeGenerator.Test/GeneratorTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+using RecipeGenerator;
+using Shouldly;
+
+namespace Tests
+{
+    public class GeneratorTest
+    {
+        [Test]
+        public void Test1()
+        {
+            var generator = new Generator();
+        }
+    }
+}
---
+++ Recipes/RecipeGenerator/Generator.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace RecipeGenerator
+{
+    public class Generator
+    {
+    }
+}
