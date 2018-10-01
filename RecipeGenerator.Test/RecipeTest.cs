f2b5d6d7b5cda8c6e909cf6df5a200f0f0c18b17
Craig Norton <c9952594@gmail.com>
2018-10-01 13:27:51 +0100

    .

--- RecipeGenerator.Test/RecipeTest.cs
+++ RecipeGenerator.Test/RecipeTest.cs
@@ -15,7 +15,7 @@ namespace Tests {
         public readonly Node Parent;
         public readonly List<Node> Children;
 
-        public Node(int index, string value, Node parent)
+        Node(int index, string value, Node parent)
         {
             this.Index = index;
             this.Value = value;
@@ -24,7 +24,7 @@ namespace Tests {
         }
 
         public static Node FromList(IEnumerable<string> lines) {
-            var root = new Node(-1, "", null);
+            var root = new Node(-1, null, null);
             var current = root;
             foreach (var line in lines)
             {
@@ -43,11 +43,23 @@ namespace Tests {
             return root;
         } 
 
-        public override string ToString() => $"{Index}: {Value}, {Children.Count}";
+        public override string ToString() {
+            var builder = new StringBuilder();
+            if (Index >= 0) {
+                builder.AppendLine($"{"".PadLeft(Index, ' ')}{Value}({Index})");
+            }
+            foreach (var child in Children)
+            {
+                builder.Append(child.ToString());
+            }
+            return builder.ToString();
+        }
     }
 
     public class RecipeTest {
-        string[] input = new [] {
+        [Test]
+        public void ShouldCreateATreeOfTheInputRecipeList() {
+            var root = Node.FromList(new [] {
                 "A",
                 " B",
                 "  C",
@@ -55,46 +67,44 @@ namespace Tests {
                 " E",
                 "  F",
                 "  G",
-            };
-
-        [Test]
-        public void SimpleGroup() {
-            var root = Node.FromList(input);
+            });
             
+            var built = root.ToString();
+
             Node a = root.Children.First();
             a.Index.ShouldBe(0);
             a.Value.ShouldBe("A");
             a.Children.Count.ShouldBe(2);
 
-            Node b = a.Children.First();
-            b.Index.ShouldBe(1);
-            b.Value.ShouldBe("B");
-            b.Children.Count.ShouldBe(2);
+                Node b = a.Children.First();
+                b.Index.ShouldBe(1);
+                b.Value.ShouldBe("B");
+                b.Children.Count.ShouldBe(2);
 
-            Node c = b.Children.First();
-            c.Index.ShouldBe(2);
-            c.Value.ShouldBe("C");
-            c.Children.Count.ShouldBe(0);
+                    Node c = b.Children.First();
+                    c.Index.ShouldBe(2);
+                    c.Value.ShouldBe("C");
+                    c.Children.Count.ShouldBe(0);
 
-            Node d = b.Children.ToArray()[1];
-            d.Index.ShouldBe(2);
-            d.Value.ShouldBe("D");
-            d.Children.Count.ShouldBe(0);
+                    Node d = b.Children.ToArray()[1];
+                    d.Index.ShouldBe(2);
+                    d.Value.ShouldBe("D");
+                    d.Children.Count.ShouldBe(0);
 
-            Node e = a.Children.ToArray()[1];
-            e.Index.ShouldBe(1);
-            e.Value.ShouldBe("E");
-            e.Children.Count.ShouldBe(2);
+                Node e = a.Children.ToArray()[1];
+                e.Index.ShouldBe(1);
+                e.Value.ShouldBe("E");
+                e.Children.Count.ShouldBe(2);
 
-            Node f = e.Children.First();
-            f.Index.ShouldBe(2);
-            f.Value.ShouldBe("F");
-            f.Children.Count.ShouldBe(0);
+                    Node f = e.Children.First();
+                    f.Index.ShouldBe(2);
+                    f.Value.ShouldBe("F");
+                    f.Children.Count.ShouldBe(0);
 
-            Node g = e.Children.ToArray()[1];
-            g.Index.ShouldBe(2);
-            g.Value.ShouldBe("G");
-            g.Children.Count.ShouldBe(0);
+                    Node g = e.Children.ToArray()[1];
+                    g.Index.ShouldBe(2);
+                    g.Value.ShouldBe("G");
+                    g.Children.Count.ShouldBe(0);
         }
     }
 }
